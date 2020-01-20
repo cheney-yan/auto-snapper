d14c6442b2eeb2f2236b10ea26b20bdea480f9e3
cheney <cheney@sondersafe.com>
2020-01-20 23:21:14 +1100

    Seems working, but would be nice to add visual debug information

--- camera.py
+++ camera.py
@@ -5,18 +5,54 @@ import logging
 from time import sleep
 
 
-def resize(original, camera, last_image=None):
+def resize(original, camera):
     width = int(original.shape[1] * camera.scale_percent / 100)
     height = int(original.shape[0] * camera.scale_percent / 100)
     dim = (width, height)
     view = cv2.resize(original, dim, interpolation=cv2.INTER_AREA)
     return view
 
-def greyify(original, camera, last_image):
+
+def greyify(original, camera):
     return cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
-def get_max_change(original, camera, last_image):
-    return None # todo: here
+
+def get_max_change(image, camera, compared):
+    # Compare the two frames, find the difference
+    frame_delta = cv2.absdiff(image, compared)
+    thresh = cv2.threshold(
+        frame_delta, camera.image_diff_threshold, 255, cv2.THRESH_BINARY)[1]
+    # Fill in holes via dilate(), and find contours of the thesholds
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts, _ = cv2.findContours(
+        thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # loop over the contours
+    movement_inf = (0, 0, 0, 0, 0)  # size, x, y w, h
+
+    for c in cnts:
+        # Save the coordinates of all found contours
+        (x, y, w, h) = cv2.boundingRect(c)
+        size = w * h
+        if size > movement_inf[0]:
+            movement_inf = (size, x, y, w, h)
+
+    # If the contour is too small, ignore it, otherwise, there's transient
+    # movement
+    if movement_inf[0] > camera.image_diff_mize_threshold:
+        return movement_inf
+    return (0, 0, 0, 0, 0)
+
+
+def get_max_change_from_start(image, camera):
+    if len(camera.buffer)>1:
+        return get_max_change(camera.buffer[-1]['grey'], camera, camera.buffer[0]['grey'])
+    return None
+
+
+def get_max_change_from_end(image, camera):
+    if len(camera.buffer)>1:
+        return get_max_change(camera.buffer[-1]['grey'], camera, camera.buffer[-2]['grey'])
+    return None
 
 
 class VideoCamera(object):
@@ -31,10 +67,14 @@ class VideoCamera(object):
         self.start_daemon()
         self.queue_size = config.get('queue_size', 10)
         self.sleep_time = config.get('sleep_time', 1)
+        self.image_diff_threshold = config.get('image_diff_threshold', 25)
+        self.image_diff_mize_threshold = config.get(
+            'image_diff_mize_threshold', 99999)
         self.plugins = {
             'view': resize,
             'grey': greyify,
-            'max_diff_size': get_max_change,
+            'change_from_start': get_max_change_from_start,
+            'change_from_last': get_max_change_from_end,
         }
 
     def __del__(self):
@@ -48,21 +88,24 @@ class VideoCamera(object):
     def run(self):
         while True:
             self.log.debug('Process one frame.')
-            self.buffer.append(self.process_frame(self.buffer[-1] if self.buffer else None))
+            self.buffer.append(self.process_frame())
             if len(self.buffer) > self.queue_size:
                 self.buffer.pop(0)
             self.log.debug('Current queue size: %s', len(self.buffer))
-
+            if len(self.buffer)>2:
+                # self.log.debug('Process result of last frame %s', self.buffer[-1])
+                self.log.debug('Change since %d frame ago: %d pixels', self.queue_size, self.buffer[-1]['change_from_start'][0])
+                self.log.debug('Change since last frame: %d ', self.buffer[-1]['change_from_last'][0])
             self.log.debug("Waiting...")
             sleep(self.sleep_time)
 
-    def process_frame(self, compared=None):
+    def process_frame(self):
         success, original = self.video.read()
         item = {
             'original': original
         }
         for key, method in self.plugins.items():
-            item[key] = method(original, self, compared) if method else None
+            item[key] = method(original, self) if method else None
         return item
 
     def get_latest_motion_frame(self):
